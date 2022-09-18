a4745f0b583ffe5cd5c64e3721f643fb1ef96ebe
Alexander Bui <alexbui831@gmail.com>
2022-09-18 12:39:54 -0500

    serve html with express

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+const port = process.env.PORT || 3000; 
 
 // http method
 // basically when we want to visit from a web browser: we get 'Welcome Home'
 app.get('/', (request, response) => {
-    response.send('Welcome home');
+    response.sendFile(path.join(__dirname, '/index.html'));
 });
-
-const port = process.env.PORT || 3000; 
 // defines webserver port with callback function
 // when we visit on terminal
 app.listen(port, () => {
