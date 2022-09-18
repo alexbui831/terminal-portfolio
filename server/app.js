ec26b1654619c113b43c3affa0ab95a5b1518755
Alexander Bui <alexbui831@gmail.com>
2022-09-18 13:48:02 -0500

    express modifications on app.js
    served my index.html with express backend code

--- server/app.js
+++ server/app.js
@@ -5,12 +5,14 @@ const app = express();
 const port = process.env.PORT || 3000; 
 
 // http method
-// basically when we want to visit from a web browser: we get 'Welcome Home'
-app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, '/index.html'));
-});
+// basically when we want to visit from a web browser
+app.use(express.static('client'));
+
+
+
+
 // defines webserver port with callback function
 // when we visit on terminal
 app.listen(port, () => {
-    console.log('Whats up');
+    console.log('Server connected at:', port);
 });
